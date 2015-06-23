941665e35b310c4df422534aae5973a83fecd020
FdevMuMu <fdevmsy@gmail.com>
2015-06-23 22:43:13 +0800

    整理了画图define,鼠标define和计时define
    abc.h中的两个宏定义开启或关闭画图和鼠标,dataprocess中测时define

--- GK_Kinect/DataProcess.cpp
+++ GK_Kinect/DataProcess.cpp
@@ -35,15 +35,19 @@ long int xx = 0;
 long int yy = 0;
 long int msy = 0;
 long int center_msy;//abc.h中返回的center,表示（x,y)那点对应在矩阵里的位置
-int pointx;
-int pointy;
+int pointx; //用于存储鼠标获得的(x,y)坐标
+int pointy; //用于存储鼠标获得的(x,y)坐标
 //const openni::DepthPixel* pDepth;
 IplImage *showxy_msy;
 IplImage *showxz_msy;
-//#define showphoto_msy//测试时显示连通域的部分,定义表示关闭,注释掉表示开启//开启时非常浪费时间,大约6ms处理一帧,关闭时只需0.5ms,定义在abc.h中
+//////不要在这个cpp里修改//////#define showphoto_msy//测试时显示连通域的部分,定义表示关闭,注释掉表示开启//开启时非常浪费时间,大约6ms处理一帧,关闭时只需0.5ms,定义在abc.h中 
+//////不要在这个cpp里修改//////#define mousedebug //重复显示一帧图像, 通过鼠标获取所指连通域的三个筛选值(面积比 长宽比 距离与面积的乘积) 注释掉表示开启 ,定义在abc.h中
+
+//#define measuretime //测连通域时间,显示在下方文本中 注释掉表示开启, 注意测时程序不能和鼠标程序同时开启
 
 ///////////////
 ////mouse click
+#ifndef mousedebug
 void on_mouse(int event, int x, int y, int flags, void* ustc)
 {
 	CvFont font;
@@ -59,16 +63,9 @@ void on_mouse(int event, int x, int y, int flags, void* ustc)
 		cvCircle(pOut02, pt, 2, cvScalar(255, 0, 0, 0), CV_FILLED, CV_AA, 0);*/
 		pointx = x; pointy = y;
 		//cvShowImage("win02", pOut02);
-
-
-
-	}
+     }
 }
-
-
-
-//////
-
+#endif
 
 
 CDataProcess::CDataProcess()
@@ -158,31 +155,34 @@ unsigned char B[DEPTH_WIDTH][DEPTH_HEIGHT];	//三维坐标点对应的彩色数据的蓝色分量
 /************************************************************************/
 /*处理未曾旋转变换的数据                                                */
 /************************************************************************/
-static int a = 0;
+
+static int initializephoto = 0;
 
 
 void CDataProcess::ProcessSrc(stWP_K_3D_Object* in3DObj)
 {
-	if (a == 0)
+	if (initializephoto == 0)//对图片的初始化只执行一次, 因为这个函数一帧一调用, 所以调用第一次后a设为1
 	{   
 #ifndef showphoto_msy
 			cvNamedWindow("win02", 0);
 #endif
-		CvSize size; //=  cvGetSize(BGR);//得到摄像头图像大小
+		CvSize size;
 		size.height = 480;
 		size.width = 640;
-		
+#ifndef showphoto_msy		
 		pOut02 = cvCreateImage(size, 8, 3);
-		//cvNamedWindow("pOut02", 0);
-        pOut01 = cvCreateImage(size, 8, 3);
+#endif
+		//cvNamedWindow("pOut02", 0);//几个并没有使用的图
+        /*pOut01 = cvCreateImage(size, 8, 3);
 		pCannyImg1 = cvCreateImage(size, 8, 3);
 
 		showxy_msy = cvCreateImage(size, 8, 3);
-		showxz_msy = cvCreateImage(size, 8, 3);
+		showxz_msy = cvCreateImage(size, 8, 3);*/
 		//cvNamedWindow("pOut01", 0);
+#ifndef mousedebug
 		cvSetMouseCallback("win02", on_mouse, NULL);
-		
-		a = 1;
+#endif
+		initializephoto = 1;
 	}
 	//////////////////////////////////////////////
 	//示例：文本输出到单行控件
@@ -193,14 +193,13 @@ void CDataProcess::ProcessSrc(stWP_K_3D_Object* in3DObj)
 	///////////////////////////////////////////////
 
 	
-
+#ifndef mousedebug
 	while (1)
 	{
+#endif
 	    char key = 0;
 	    int i = 0, j = 0;
 		long int temp = 0;
-		//tempp = (unsigned short*)in3DObj->depthD16[DEPTH_WIDTH*DEPTH_HEIGHT];
-
 		for (i = 0; i < 480; i++)
 		{
 			for (j = 0; j<640; j++)
@@ -221,16 +220,7 @@ void CDataProcess::ProcessSrc(stWP_K_3D_Object* in3DObj)
 
 			}
 		}
-		
-		
-
-
-
-
-
-		
-
-
+#ifndef measuretime		
 			//********************
 			//********************测试时间的起始点
 			LARGE_INTEGER Frequency, CountEnd, CountStart;
@@ -240,44 +230,39 @@ void CDataProcess::ProcessSrc(stWP_K_3D_Object* in3DObj)
 			double dfElapseMS1 = 0;
 			double dfElapseMS2 = 0;
 			double dfElapseMS3 = 0;
+#endif
+
 
 			bool_max_connectivity_analyze2_1_OBJ();
-
+#ifndef measuretime	
 			//****************************************************************************************************
 			QueryPerformanceCounter(&CountEnd);
 			dfElapseMS = (double)((double)(CountEnd.QuadPart - CountStart.QuadPart + 10) / (double)Frequency.QuadPart)*1000.0;
 			//****************************************************************************************************第一个时间节点
 
-			//**************************//mouse click
+			
 
 
-			//////////////////////////////////////////////
-			/*示例：文本输出到单行控件*/
+			//////////////////////////////////////////////测试连通域计算的时间并输出在下方文本框中
 			CString strTmp;
-			/*int a = 1;*/
-//			strTmp.Format(L"正在处理原始数据 time= %f", dfElapseMS);
-//			PrintDis(strTmp);
+			strTmp.Format(L"正在处理原始数据 time= %f", dfElapseMS);
+			PrintDis(strTmp);
 			///////////////////////////////////////////////
-
-
-			//SYSTEMTIME sys2; GetLocalTime(&sys2);
-
-			/*	CString s1;
-
-				s1.Format(_T("%f"), dfElapseMS);
-				AfxMessageBox(s1);*/
+#endif
 
 #ifndef showphoto_msy
 			cvShowImage("win02", pOut02);
-			//cvWaitKey(10);
+			cvWaitKey(10);
+#ifndef mousedebug
 			if (cvWaitKey(20) == 'N')
 				return;
-
+#endif
 
 
 #endif
-//			Sleep(100);
+#ifndef mousedebug
 		}
+#endif
 
 
 
--- GK_Kinect/abc.h
+++ GK_Kinect/abc.h
@@ -25,14 +25,15 @@ float AXc = 0, AYc = 0, Xc = 0, Yc = 0, weight_msy= 0;//////Ã¿¸öXÓë´ËµãÖÊÁ¿µÄ³Ë»
 extern long int center_msy;//abc.hÖĞ·µ»ØµÄcenter,±íÊ¾£¨x,y)ÄÇµã¶ÔÓ¦ÔÚ¾ØÕóÀïµÄÎ»ÖÃ
 //extern const openni::DepthPixel* pDepth;
 char str[50];
-extern int pointx;
+extern int pointx;//ÓÃÓÚ´æ´¢Êó±ê»ñµÃµÄ(x,y)×ø±ê
 extern int pointy;
 /////////////////////////////////MSY TEST////////////////////////////
 extern IplImage *showxy_msy;
 extern IplImage *showxz_msy;
 int pointxy = 1;
-//extern int showphoto_msy;
-//#define showphoto_msy//²âÊÔÊ±ÏÔÊ¾Á¬Í¨ÓòµÄ²¿·Ö,¶¨Òå±íÊ¾¹Ø±Õ,×¢ÊÍµô±íÊ¾¿ªÆô//¿ªÆôÊ±·Ç³£ÀË·ÑÊ±¼ä,´óÔ¼6ms´¦ÀíÒ»Ö¡,¹Ø±ÕÊ±Ö»Ğè0.5ms
+
+#define showphoto_msy//²âÊÔÊ±ÏÔÊ¾Á¬Í¨ÓòµÄ²¿·Ö,¶¨Òå±íÊ¾¹Ø±Õ,×¢ÊÍµô±íÊ¾¿ªÆô//¿ªÆôÊ±·Ç³£ÀË·ÑÊ±¼ä,´óÔ¼6ms´¦ÀíÒ»Ö¡,¹Ø±ÕÊ±Ö»Ğè0.5ms
+#define mousedebug //ÖØ¸´ÏÔÊ¾Ò»Ö¡Í¼Ïñ, Í¨¹ıÊó±ê»ñÈ¡ËùÖ¸Á¬Í¨ÓòµÄÈı¸öÉ¸Ñ¡Öµ(Ãæ»ı±È ³¤¿í±È ¾àÀëÓëÃæ»ıµÄ³Ë»ı) ×¢ÊÍµô±íÊ¾¿ªÆô
 void DrawPoint(int x, int y, unsigned char R, unsigned char G, unsigned char B, IplImage *img)
 {
 	if (x>img->width)		x = img->width;
@@ -108,20 +109,11 @@ void clean(IplImage *img)
 }
 
 
-int x=0, y=0;
-//VOID TEST()
-//{
-//	x++;
-//	if (x > 200)
-//		x = 0;
-//	DrawPoint(x, 100, 255, 0, 0, pOut02);
-//
-//}
+
 
 long int bool_max_connectivity_analyze2_1_OBJ()
 {
-	/*TEST();*/
-	/*return 1;*/
+	
 	long int center = 0;
 
 	register unsigned long int  boolset = 3000;//ãĞÖµ
@@ -130,10 +122,14 @@ long int bool_max_connectivity_analyze2_1_OBJ()
 
 
 	int Z_min_diff = Z_min_diff_set;//50;	//ÕâÊÇÒ»¸ö¿Éµ÷Öµ¹ØÏµµ½Éî¶ÈµÄ·Ö±æ´ÖÂÔ³Ì¶È
+#ifdef showphoto_msy
+	const char X_step = 2;		//ÏñËØµãµÄ²½³¤ ¸ôµãÉ¨Ãè ÓÃÓÚ¼õÉÙÔËËãÁ¿
+	const char Y_step = 2;		//ÏñËØµãµÄ²½³¤ ¸ôµãÉ¨Ãè ÓÃÓÚ¼õÉÙÔËËãÁ¿
+#endif
+#ifndef  showphoto_msy
 	const char X_step = 1;		//ÏñËØµãµÄ²½³¤ ¸ôµãÉ¨Ãè ÓÃÓÚ¼õÉÙÔËËãÁ¿
-	const char Y_step = 1;		//ÏñËØµãµÄ²½³¤ ¸ôµãÉ¨Ãè ÓÃÓÚ¼õÉÙÔËËãÁ¿
-
-
+	const char Y_step = 1;
+#endif
 	const int x320 = DEPTH_VISION_CAM_WIDTH;//320;//320;//´¦ÀíÍ¼ÏñÊı¾İµÄ´óĞ¡ÉèÖÃ  xÖµ±ØĞëºÍÔ­Êı¾İÏàÍ¬·ñÔò³öÏÖ ĞĞ´íÎó 
 	const int y240 = DEPTH_VISION_CAM_HEIGHT;//240;//240;//´¦ÀíÍ¼ÏñÊı¾İµÄ´óĞ¡ÉèÖÃ
 	const int datalong = DEPTH_VISION_CAM_WIDTH*DEPTH_VISION_CAM_HEIGHT;
@@ -350,16 +346,16 @@ long int bool_max_connectivity_analyze2_1_OBJ()
 
 			int t;
 			unsigned char R, G, B;
-
+#ifndef mousedebug
 			int maosdisplaynumber = -DepthBuf_O[pointy][pointx];
-
+#endif
 
 			if (1)//ËùÓĞÁ¬Í¨Óò Öğ¸öÏÔÊ¾
 			{
 				register int   min_x, max_x, min_y, max_y;
 				i = 1;
 #ifndef showphoto_msy
-					clean(pOut01);//ÇåÀíÁ½¸öÍ¼
+					//clean(pOut01);//ÇåÀíÁ½¸öÍ¼
 					clean(pOut02);
 					//////ÇåÀíÓÒÉÏ½ÇµÄºÚÍ¼
 					int x, y;
@@ -374,10 +370,6 @@ long int bool_max_connectivity_analyze2_1_OBJ()
 					}
 #endif
 			  /////////
-
-
-
-
 
 				for (j = 2; j <= connect_area_num; j++)//j Á¬Í¨ÓòµÄ¸öÊı
 				{
@@ -448,35 +440,33 @@ long int bool_max_connectivity_analyze2_1_OBJ()
 								//{	//Ô²µÄ³¤¿í±ÈÎª1  Ô²µÄÃæ»ıÓëÍâ½Ó×îĞ¡¾ØĞÍÃæ»ı±ÈÎª 2*r*2*r/Pi*r*r =4/pi 					
 								//cout << "³¤¿í±È" << P_xy << " Ãæ»ı±È" << P_area << " Êµ¼ÊÃæ»ı " << connect_area_s_e_w[j][2] * X_step*Y_step << "ÖĞĞÄµã¾àÀë" << DepthBuf_O_msy[(max_y + min_y)/2][(max_x + min_x) / 2] << endl;
 								/////////////////////////////////////////////////////ÒÔÉÏÄÚÈİÊä³ö³¤¿í±È£¬Ãæ»ı±È£¬Êµ¼ÊÃæ»ı£¬ÖĞĞÄ¾àÀë////////////////////////////
-							if (1)//j == maosdisplaynumber)
+#ifndef mousedebug
+							if (1)
 							{
+#endif
 								for (i = connect_area_s_e_w[j][0]; i < connect_area_s_e_w[j][1]; i++)
 								{
-
-
-									/*if (pointx<max_x&&pointx>min_x&&pointy<max_y&&pointy>min_y)
-									{*/
 #ifndef showphoto_msy
-										DrawPoint(area_grow_data_obj[i][1], area_grow_data_obj[i][0], R, G, 0, pOut02);//SetColor(y,x,cPoint);//²âÊÔ´úÂë
-										if (area_grow_data_obj[i][1] % 2 == 0 && area_grow_data_obj[i][0] % 2 == 0)
-										{
-											//dataShow[area_grow_data_obj[i][0] * 320 * 3 + area_grow_data_obj[i][1] * 3] = 0xff;
+									DrawPoint(area_grow_data_obj[i][1], area_grow_data_obj[i][0], R, G, 0, pOut02);//SetColor(y,x,cPoint);//²âÊÔ´úÂë
 
-											dataShow[((240 - area_grow_data_obj[i][0] / 2) * 320 + area_grow_data_obj[i][1] / 2) * 3] = R;
-											dataShow[((240 - area_grow_data_obj[i][0] / 2) * 320 + area_grow_data_obj[i][1] / 2) * 3 + 1] = G;
-											dataShow[((240 - area_grow_data_obj[i][0] / 2) * 320 + area_grow_data_obj[i][1] / 2) * 3 + 2] = B;
-										}
-#endif
+									if (area_grow_data_obj[i][1] % 2 == 0 && area_grow_data_obj[i][0] % 2 == 0)
+									{
+										//ÒòÎªÓÒÉÏ½ÇµÄÍ¼Ö»ÓĞ320*240, ËùÒÔ¸ôÒ»¸öµãÒ»»­Í¼
+										dataShow[((240 - area_grow_data_obj[i][0] / 2) * 320 + area_grow_data_obj[i][1] / 2) * 3] = R;
+										dataShow[((240 - area_grow_data_obj[i][0] / 2) * 320 + area_grow_data_obj[i][1] / 2) * 3 + 1] = G;
+										dataShow[((240 - area_grow_data_obj[i][0] / 2) * 320 + area_grow_data_obj[i][1] / 2) * 3 + 2] = B;
 									}
-
-
+#endif
 
 
 									center = (max_x + min_x) / 2 + (max_y + min_y) * 320;
-									/*}*/
-								/*}*/
+								}
+								
+#ifndef mousedebug
 							}
+#endif
 								///////////////////////////////°Ñ×ø±êµÈ±ê¼ÇÔÚÍ¼ÉÏMSY///////////////////////////////////////////
+#ifndef mousedebug
 							if (j == maosdisplaynumber)
 							{
 								CvFont font;
@@ -489,6 +479,7 @@ long int bool_max_connectivity_analyze2_1_OBJ()
 								sprintf(str, "  end  %f  ",  ((float)(weight_msy / (float)connect_area_s_e_w[j][2])*sqrt((connect_area_s_e_w[j][2] * X_step*Y_step))));// DepthBuf_O_msy[(int)Yc][(int)Xc]);
 								cvPutText(pOut02, str, cvPoint(min_x + 10, min_y + 50), &font, CV_RGB(255, 255, 255));
 							}
+#endif
 								//								sprintf(str, "  end  %f  ", weight_msy / (float)((float)connect_area_s_e_w[j][2])* sqrt((connect_area_s_e_w[j][2] * X_step*Y_step)));// DepthBuf_O_msy[(int)Yc][(int)Xc]);
 
 /*
@@ -581,18 +572,18 @@ long int bool_max_connectivity_analyze2_1_OBJ()
 
 
 			
-			if (0)// display_process_image == 1)//ÅĞ¶Ï ÏÔÊ¾¹ı³ÌÍ¼Ïñ	//ÏÔÊ¾×î´óÁ¬Í¨Óò
-			for (y = 0; y<DEPTH_VISION_CAM_HEIGHT; y++)
-			for (x = 0; x<DEPTH_VISION_CAM_WIDTH; x++)
-			{
-				if (DepthBuf_O[y][x] == 1)
-					DrawPoint(x, y, 0, 255, 255, pOut01);//SetColor(y,x,cPoint);//²âÊÔ´úÂë
-				else
-					DrawPoint(x, y, 0, 0, 0, pOut01);//SetColor(y,x,cPoint);//²âÊÔ´úÂë
+			//if (0)// display_process_image == 1)//ÅĞ¶Ï ÏÔÊ¾¹ı³ÌÍ¼Ïñ	//ÏÔÊ¾×î´óÁ¬Í¨Óò
+			//for (y = 0; y<DEPTH_VISION_CAM_HEIGHT; y++)
+			//for (x = 0; x<DEPTH_VISION_CAM_WIDTH; x++)
+			//{
+			//	if (DepthBuf_O[y][x] == 1)
+			//		DrawPoint(x, y, 0, 255, 255, pOut01);//SetColor(y,x,cPoint);//²âÊÔ´úÂë
+			//	else
+			//		DrawPoint(x, y, 0, 0, 0, pOut01);//SetColor(y,x,cPoint);//²âÊÔ´úÂë
 
-			}
+			//}
 
-			//			cvShowImage("win01", pOut01);
+			////			cvShowImage("win01", pOut01);
 			return center;
 
 		}
